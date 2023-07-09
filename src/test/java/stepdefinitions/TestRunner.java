6950a76446ea03da127f5c1960270dd47f23e4da
dejanvajagich <96501576+dejanvajagich@users.noreply.github.com>
2023-07-09 13:25:21 +0200

    Update TestRunner.java

--- src/test/java/stepdefinitions/TestRunner.java
+++ src/test/java/stepdefinitions/TestRunner.java
@@ -1,7 +1,4 @@
 package stepdefinitions;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
